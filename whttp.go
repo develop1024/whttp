239df1ba2454919398c4e0867c417f0e2c4e8fbd
develop1024 <656562721@qq.com>
2020-12-19 13:41:05 +0800

    update

--- whttp.go
+++ whttp.go
@@ -2,15 +2,17 @@ package whttp
 
 import (
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"net/http"
 	"net/url"
 	"strconv"
 	"strings"
 )
-
-// 自定义参数map
-type CMap map[string]interface{}
+// 参数别名
+type Params map[string]interface{}
+// 请求头别名
+type Headers map[string]interface{}
 
 // 请求结构体
 type Request struct {}
@@ -39,7 +41,7 @@ func (resp *Response) ToString() string {
 	return string(resp.Resp)
 }
 
-// Get请求
+// 普通GET请求
 func (r *Request) Get(URL string) *Response {
 	resp, err := http.Get(URL)
 	if err != nil {
@@ -65,61 +67,7 @@ func (r *Request) Get(URL string) *Response {
 	}
 }
 
-// 自定义Get请求
-func (r *Request) CustomGet(URL string, params CMap, headers ...CMap) *Response {
-	request, err := http.NewRequest("GET", URL, nil)
-	if err != nil {
-		return &Response{
-			Resp:  nil,
-			Error: err,
-		}
-	}
-
-	// 设置请求参数
-	q := request.URL.Query()
-	for key, val := range params {
-		q.Add(key, ToStrType(val))
-	}
-
-	// 构造请求参数赋值给请求url
-	request.URL.RawQuery = q.Encode()
-
-	// 添加请求头
-	request.Header.Add("cache-control", "no-cache")
-	// 设置请求头
-	request.Header.Set("content-type", "application/x-www-form-urlencoded")
-
-	for _, hs := range headers {
-		for key, val := range hs {
-			request.Header.Add(key, ToStrType(val))
-		}
-	}
-
-	resp, err := http.DefaultClient.Do(request)
-	if err != nil {
-		return &Response{
-			Resp:  nil,
-			Error: err,
-		}
-	}
-
-	response, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return &Response{
-			Resp:  nil,
-			Error: err,
-		}
-	}
-	defer resp.Body.Close()
-
-	return &Response{
-		Resp:  response,
-		Error: nil,
-	}
-}
-
-
-// Post请求
+// 普通POST请求
 func (r *Request) Post(URL string) *Response {
 	resp, err := http.Post(URL, "application/json", nil)
 	if err != nil {
@@ -144,200 +92,6 @@ func (r *Request) Post(URL string) *Response {
 	}
 }
 
-// 自定义Post请求
-func (r *Request) CustomPost(URL string, params CMap, headers ...CMap) *Response {
-
-	u := url.Values{}
-	for key, val := range params {
-		u.Add(key, ToStrType(val))
-	}
-
-	payload := strings.NewReader(u.Encode())
-	request, err := http.NewRequest("POST", URL, payload)
-	if err != nil {
-		return &Response{
-			Resp:  nil,
-			Error: err,
-		}
-	}
-
-	// 设置请求头
-	request.Header.Set("Content-Type", "application/x-www-form-urlencoded")
-	for _, hs := range headers {
-		for key, val := range hs {
-			request.Header.Add(key, ToStrType(val))
-		}
-	}
-
-	resp, err := http.DefaultClient.Do(request)
-	if err != nil {
-		return &Response{
-			Resp:  nil,
-			Error: err,
-		}
-	}
-	defer resp.Body.Close()
-
-	response, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return &Response{
-			Resp:  nil,
-			Error: err,
-		}
-	}
-
-	return &Response{
-		Resp:  response,
-		Error: nil,
-	}
-}
-
-
-// 自定义Put请求
-func (r *Request) CustomPut(URL string, params CMap, headers ...CMap) *Response {
-
-	u := url.Values{}
-	for key, val := range params {
-		u.Add(key, ToStrType(val))
-	}
-
-	payload := strings.NewReader(u.Encode())
-	request, err := http.NewRequest("PUT", URL, payload)
-	if err != nil {
-		return &Response{
-			Resp:  nil,
-			Error: err,
-		}
-	}
-
-	// 设置请求头
-	request.Header.Set("Content-Type", "application/x-www-form-urlencoded")
-	for _, hs := range headers {
-		for key, val := range hs {
-			request.Header.Add(key, ToStrType(val))
-		}
-	}
-
-	resp, err := http.DefaultClient.Do(request)
-	if err != nil {
-		return &Response{
-			Resp:  nil,
-			Error: err,
-		}
-	}
-	defer resp.Body.Close()
-
-	response, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return &Response{
-			Resp:  nil,
-			Error: err,
-		}
-	}
-
-	return &Response{
-		Resp:  response,
-		Error: nil,
-	}
-}
-
-
-// 自定义Delete请求
-func (r *Request) CustomDelete(URL string, params CMap, headers ...CMap) *Response {
-
-	u := url.Values{}
-	for key, val := range params {
-		u.Add(key, ToStrType(val))
-	}
-
-	payload := strings.NewReader(u.Encode())
-	request, err := http.NewRequest("DELETE", URL, payload)
-	if err != nil {
-		return &Response{
-			Resp:  nil,
-			Error: err,
-		}
-	}
-
-	// 设置请求头
-	request.Header.Set("Content-Type", "application/x-www-form-urlencoded")
-	for _, hs := range headers {
-		for key, val := range hs {
-			request.Header.Add(key, ToStrType(val))
-		}
-	}
-
-	resp, err := http.DefaultClient.Do(request)
-	if err != nil {
-		return &Response{
-			Resp:  nil,
-			Error: err,
-		}
-	}
-	defer resp.Body.Close()
-
-	response, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return &Response{
-			Resp:  nil,
-			Error: err,
-		}
-	}
-
-	return &Response{
-		Resp:  response,
-		Error: nil,
-	}
-}
-
-// 自定义PATCH请求
-func (r *Request) CustomPatch(URL string, params CMap, headers ...CMap) *Response {
-
-	u := url.Values{}
-	for key, val := range params {
-		u.Add(key, ToStrType(val))
-	}
-
-	payload := strings.NewReader(u.Encode())
-	request, err := http.NewRequest("PATCH", URL, payload)
-	if err != nil {
-		return &Response{
-			Resp:  nil,
-			Error: err,
-		}
-	}
-
-	// 设置请求头
-	request.Header.Set("Content-Type", "application/x-www-form-urlencoded")
-	for _, hs := range headers {
-		for key, val := range hs {
-			request.Header.Add(key, ToStrType(val))
-		}
-	}
-
-	resp, err := http.DefaultClient.Do(request)
-	if err != nil {
-		return &Response{
-			Resp:  nil,
-			Error: err,
-		}
-	}
-	defer resp.Body.Close()
-
-	response, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return &Response{
-			Resp:  nil,
-			Error: err,
-		}
-	}
-
-	return &Response{
-		Resp:  response,
-		Error: nil,
-	}
-}
-
 // 类型转换为 string
 func ToStrType(v interface{}) string {
 	switch v.(type) {
@@ -353,5 +107,344 @@ func ToStrType(v interface{}) string {
 		return strconv.FormatBool(v.(bool))
 	default:
 		return v.(string)
+	}
+}
+
+// GET请求支持自定义参数和请求头
+func (r *Request) GetRequest(URL string, v ...interface{}) *Response {
+	request, err := http.NewRequest("GET", URL, nil)
+	if err != nil {
+		return &Response{
+			Resp:  nil,
+			Error: err,
+		}
+	}
+
+	for _, item := range v {
+		switch item.(type) {
+		case Params:
+			// 设置请求参数
+			q := request.URL.Query()
+			for key, val := range item.(Params) {
+				q.Add(key, ToStrType(val))
+			}
+
+			// 构造请求参数赋值给请求url
+			request.URL.RawQuery = q.Encode()
+
+			for key, child := range item.(Params) {
+				fmt.Println(key, child)
+				q.Add(key, ToStrType(child))
+			}
+		case Headers:
+			// 添加请求头
+			request.Header.Add("cache-control", "no-cache")
+			// 设置请求头
+			request.Header.Set("content-type", "application/x-www-form-urlencoded")
+
+			for key, val := range item.(Headers) {
+				request.Header.Add(key, ToStrType(val))
+			}
+		default:
+
+		}
+	}
+
+	resp, err := http.DefaultClient.Do(request)
+	if err != nil {
+		return &Response{
+			Resp:  nil,
+			Error: err,
+		}
+	}
+
+	response, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return &Response{
+			Resp:  nil,
+			Error: err,
+		}
+	}
+	defer resp.Body.Close()
+
+	return &Response{
+		Resp:  response,
+		Error: nil,
+	}
+}
+
+// POST请求支持自定义参数和请求头
+func (r *Request) PostRequest(URL string, v ...interface{}) *Response {
+	u := url.Values{}
+	payload := strings.NewReader(u.Encode())
+	request, err := http.NewRequest("POST", URL, payload)
+	if err != nil {
+		return &Response{
+			Resp:  nil,
+			Error: err,
+		}
+	}
+
+	for _, item := range v {
+		switch item.(type) {
+		case Params:
+			// 设置URL请求参数
+			q := request.URL.Query()
+			for key, val := range item.(Params) {
+				q.Add(key, ToStrType(val))
+			}
+
+			// 构造请求参数赋值给请求url
+			request.URL.RawQuery = q.Encode()
+
+			for key, child := range item.(Params) {
+				fmt.Println(key, child)
+				q.Add(key, ToStrType(child))
+			}
+
+			// 添加 body 请求参数
+			for key, val := range item.(Params) {
+				u.Add(key, ToStrType(val))
+			}
+		case Headers:
+			// 设置请求头
+			request.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+			for key, val := range item.(Headers) {
+				request.Header.Add(key, ToStrType(val))
+			}
+		default:
+
+		}
+	}
+
+	// 发送请求
+	resp, err := http.DefaultClient.Do(request)
+	if err != nil {
+		return &Response{
+			Resp:  nil,
+			Error: err,
+		}
+	}
+	defer resp.Body.Close()
+
+	// 读取请求
+	response, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return &Response{
+			Resp:  nil,
+			Error: err,
+		}
+	}
+
+	return &Response{
+		Resp:  response,
+		Error: nil,
+	}
+}
+
+// PUT请求支持自定义参数和请求头
+func (r *Request) PutRequest(URL string, v ...interface{}) *Response {
+	u := url.Values{}
+	payload := strings.NewReader(u.Encode())
+	request, err := http.NewRequest("PUT", URL, payload)
+	if err != nil {
+		return &Response{
+			Resp:  nil,
+			Error: err,
+		}
+	}
+
+	for _, item := range v {
+		switch item.(type) {
+		case Params:
+			// 设置URL请求参数
+			q := request.URL.Query()
+			for key, val := range item.(Params) {
+				q.Add(key, ToStrType(val))
+			}
+
+			// 构造请求参数赋值给请求url
+			request.URL.RawQuery = q.Encode()
+
+			for key, child := range item.(Params) {
+				fmt.Println(key, child)
+				q.Add(key, ToStrType(child))
+			}
+
+			// 添加 body 请求参数
+			for key, val := range item.(Params) {
+				u.Add(key, ToStrType(val))
+			}
+		case Headers:
+			// 设置请求头
+			request.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+			for key, val := range item.(Headers) {
+				request.Header.Add(key, ToStrType(val))
+			}
+		default:
+
+		}
+	}
+
+	// 发送请求
+	resp, err := http.DefaultClient.Do(request)
+	if err != nil {
+		return &Response{
+			Resp:  nil,
+			Error: err,
+		}
+	}
+	defer resp.Body.Close()
+
+	// 读取请求
+	response, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return &Response{
+			Resp:  nil,
+			Error: err,
+		}
+	}
+
+	return &Response{
+		Resp:  response,
+		Error: nil,
+	}
+}
+
+// DELETE请求支持自定义参数和请求头
+func (r *Request) DeleteRequest(URL string, v ...interface{}) *Response {
+	u := url.Values{}
+	payload := strings.NewReader(u.Encode())
+	request, err := http.NewRequest("DELETE", URL, payload)
+	if err != nil {
+		return &Response{
+			Resp:  nil,
+			Error: err,
+		}
+	}
+
+	for _, item := range v {
+		switch item.(type) {
+		case Params:
+			// 设置URL请求参数
+			q := request.URL.Query()
+			for key, val := range item.(Params) {
+				q.Add(key, ToStrType(val))
+			}
+
+			// 构造请求参数赋值给请求url
+			request.URL.RawQuery = q.Encode()
+
+			for key, child := range item.(Params) {
+				fmt.Println(key, child)
+				q.Add(key, ToStrType(child))
+			}
+
+			// 添加 body 请求参数
+			for key, val := range item.(Params) {
+				u.Add(key, ToStrType(val))
+			}
+		case Headers:
+			// 设置请求头
+			request.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+			for key, val := range item.(Headers) {
+				request.Header.Add(key, ToStrType(val))
+			}
+		default:
+
+		}
+	}
+
+	// 发送请求
+	resp, err := http.DefaultClient.Do(request)
+	if err != nil {
+		return &Response{
+			Resp:  nil,
+			Error: err,
+		}
+	}
+	defer resp.Body.Close()
+
+	// 读取请求
+	response, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return &Response{
+			Resp:  nil,
+			Error: err,
+		}
+	}
+
+	return &Response{
+		Resp:  response,
+		Error: nil,
+	}
+}
+
+// PATCH请求支持自定义参数和请求头
+func (r *Request) PatchRequest(URL string, v ...interface{}) *Response {
+	u := url.Values{}
+	payload := strings.NewReader(u.Encode())
+	request, err := http.NewRequest("PATCH", URL, payload)
+	if err != nil {
+		return &Response{
+			Resp:  nil,
+			Error: err,
+		}
+	}
+
+	for _, item := range v {
+		switch item.(type) {
+		case Params:
+			// 设置URL请求参数
+			q := request.URL.Query()
+			for key, val := range item.(Params) {
+				q.Add(key, ToStrType(val))
+			}
+
+			// 构造请求参数赋值给请求url
+			request.URL.RawQuery = q.Encode()
+
+			for key, child := range item.(Params) {
+				fmt.Println(key, child)
+				q.Add(key, ToStrType(child))
+			}
+
+			// 添加 body 请求参数
+			for key, val := range item.(Params) {
+				u.Add(key, ToStrType(val))
+			}
+		case Headers:
+			// 设置请求头
+			request.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+			for key, val := range item.(Headers) {
+				request.Header.Add(key, ToStrType(val))
+			}
+		default:
+
+		}
+	}
+
+	// 发送请求
+	resp, err := http.DefaultClient.Do(request)
+	if err != nil {
+		return &Response{
+			Resp:  nil,
+			Error: err,
+		}
+	}
+	defer resp.Body.Close()
+
+	// 读取请求
+	response, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return &Response{
+			Resp:  nil,
+			Error: err,
+		}
+	}
+
+	return &Response{
+		Resp:  response,
+		Error: nil,
 	}
 }
